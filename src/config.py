f41bdc662f60f74ea705e1caf3773c9ed772eb4b
Daniel Shmul <danielshmul@Daniels-MacBook-Pro.local>
2023-03-12 21:37:52 +0200

    Moved game source files to src

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# Display settings
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 700
+TILE_SCALE = 0.85
+TITLE = "Tic Tac Toe"
+FPS = 60
+
+# Preset colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (200, 200, 200)
