566ff5aed42345c9c73710ba9c24551f514328de
Daniel Shmul <danielshmul@Daniels-MacBook-Pro.local>
2023-03-13 11:18:04 +0200

    Added scoreboard methods

--- api/api.py
+++ api/api.py
@@ -20,8 +20,9 @@ class User(db.Model):
     name = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
 
-class Scoreboard(db.Model):
-    user_id = db.Column(db.String(100), primary_key=True)
+class Score(db.Model): #TODO: change to "Score"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(100))
     date = db.Column(db.String())
     score = db.Column(db.Integer)
     grid_size = db.Column(db.Integer, nullable=False) # 3: 3x3, 10: 10x10
@@ -42,8 +43,8 @@ def token_required(f):
         if not token:
             return jsonify({'message': 'Token is missing.'}, 401)
         
-        try:
-            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+        try: # TODO: fix error
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"]) # resulting in jwt.exceptions.ExpiredSignatureError: Signature has expired
             current_user = User.query.filter_by(public_id=data['public_id']).first()
         except:
             return jsonify({'message', 'Token is invalid.'}, 401)
@@ -129,7 +130,7 @@ def login():
 def add_score(current_user):
     data = request.get_json() # expects {"score": <score>, "grid_size": <grid_size>} 
 
-    new_score = Scoreboard(user_id=current_user.public_id, date=datetime.datetime.now(), score=data['score'], grid_size=data['grid_size'])
+    new_score = Score(user_id=current_user.public_id, date=datetime.datetime.now(), score=data['score'], grid_size=data['grid_size'])
     db.session.add(new_score)
     db.session.commit()
 
@@ -137,7 +138,7 @@ def add_score(current_user):
 
 @app.route('/score', methods=['GET'])
 def get_all_scores():
-    scores = Scoreboard.query.all()
+    scores = Score.query.all()
 
     output = []
     for score in scores:
@@ -148,8 +149,54 @@ def get_all_scores():
         score_data['grid_size'] = score.grid_size
         output.append(score_data)
 
-    return jsonify({'users': output})
+    return jsonify({'scores': output})
 
+@app.route('/score/<grid_size>', methods=['GET'])
+@token_required
+def get_user_score(current_user, grid_size):
+    scores = Score.query.filter_by(user_id=current_user.public_id, grid_size=grid_size).order_by(Score.date.desc()).all()
+
+    output = []
+    for score in scores:
+        score_data = {}
+        score_data['date'] = score.date
+        score_data['score'] = score.score
+        output.append(score_data)
+
+    return jsonify({'scores': output})
+
+@app.route('/scoreboard/<grid_size>', methods=['GET']) # scoreboard defines queries with join
+def get_grid_scoreboard(grid_size):
+    entries = db.session.query(Score.user_id, Score.date, Score.score, Score.grid_size, User.public_id, User.name)\
+        .select_from(Score).join(User, Score.user_id==User.public_id).order_by(Score.score.desc()).limit(10).all()
+
+    if not entries:
+        return jsonify({'message': 'No entries found.'})
+
+    output = []
+    for entry in entries:
+        entry_data = {}
+        entry_data['user_id'] = entry.user_id
+        entry_data['user_name'] = entry.name
+        entry_data['date'] = entry.date
+        entry_data['score'] = entry.score
+        output.append(entry_data)
+
+    return jsonify({'scoreboard': output})
+
+
+@app.route('/score/<user_id>', methods=['DELETE'])
+def delete_user_entries(user_id):
+    entries = Score.query.filter_by(user_id=user_id).all()
+
+    if not entries:
+        return jsonify({'message': 'No user entries found.'})
+
+    for entry in entries:
+        db.session.delete(entry)
+    db.session.commit()
+
+    return jsonify({'message': 'User entries deleted.'})
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
