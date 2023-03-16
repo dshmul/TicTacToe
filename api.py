a396c673b520f9293671e96d9e8ac407a02d2c30
Daniel Shmul <danielshmul@daniels-mbp.mynet>
2023-03-16 18:39:45 +0200

    Set token timeout to 30 mins

--- api.py
+++ api.py
@@ -132,7 +132,7 @@ def login():
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login Requred."'})
     
     if check_password_hash(user.password, auth.password):
-        token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=.1)}, \
+        token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, \
                            app.config['SECRET_KEY'])
 
         return jsonify({'token': token})
